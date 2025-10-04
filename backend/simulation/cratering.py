28f0b869e6617c97ce419c0e8f2ebcf512f2658c
MarcelFeo <marcelfeo29@gmail.com>
2025-10-04 20:32:15 -0300

    feat: add crater_depth function

--- backend/simulation/cratering.py
+++ backend/simulation/cratering.py
@@ -8,14 +8,12 @@ import math
 from data_services.nasa_client import get_asteroid_data
 from simulation.energetics import calculate_impact_energy
 
-# constants
-k = 1.8
-
-def crater_diameter(asteroid_id: id):
+def crater_diameter(asteroid_id: id): # metros
     """
     k: empirical constant (~1.8 for rock)
     """
-
+    
+    k = 1.8
     data = get_asteroid_data(asteroid_id)
 
     if data:
@@ -24,8 +22,19 @@ def crater_diameter(asteroid_id: id):
             D_c = k * (energy_results['energy_megatons_tnt'] ** (1/3.4)) * 1000
             return D_c
 
+def crater_depth(asteroid_id: id): # metros
+    """
+    Estimates the crater depth
+    """
 
+    data = get_asteroid_data(asteroid_id)
+
+    if data:
+        energy_results = calculate_impact_energy(data)
+        if energy_results:
+            return 0.2 * energy_results['mean_diameter_m']
 
 # Standalone test block for this module
 if __name__ == "__main__":
     print(crater_diameter("3542519"))
+    print(crater_depth("3542519"))
